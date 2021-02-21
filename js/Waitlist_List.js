cea694a6f088141842d9ece1016dc5624604ebc6
Tess Guefen <tguefen@miva.com>
2021-02-21 11:16:16 -0800

    WIP, typo fixes

--- js/Waitlist_List.js
+++ js/Waitlist_List.js
@@ -1,4 +1,5 @@
-function Waitlist_Load_Query( filter, sort, offset, count, callback, delegator ) {
+function Waitlist_Load_Query( filter, sort, offset, count, callback, delegator )
+{
 	return AJAX_Call_Module_JSON( callback, 'admin', 'TGWaitlist', 'Waitlist_Load_Query',
 	{
 		Filter:			filter,
@@ -8,21 +9,24 @@ function Waitlist_Load_Query( filter, sort, offset, count, callback, delegator )
 	}, delegator );
 }
 
-function Waitlist_Batchlist_Delete( id, callback, delegator ) {
+function Waitlist_Batchlist_Delete( id, callback, delegator )
+{
 	return AJAX_Call_Module_JSON( callback, 'admin', 'TGWaitlist', 'Waitlist_Delete',
 	{
 		ID: id
 	}, delegator );
 }
 
-function TriggerEmails_Execute( waitlist_items, callback, delegator ) {
+function TriggerEmails_Execute( waitlist_items, callback, delegator )
+{
 	return AJAX_Call_Module_JSON( callback, 'admin', 'TGWaitlist', 'Waitlist_Trigger_Emails',
 	{
 		Waitlists:	waitlist_items
 	}, delegator );
 }
 
-function Waitlist_Batchlist() {
+function Waitlist_Batchlist()
+{
 	MMBatchList.call( this, 'jsWaitlist_Batchlist' );
 
 	this.Feature_SearchBar_SetPlaceholderText( 'Search Waitlists...' );
@@ -48,36 +52,42 @@ DeriveFrom( MMBatchList, Waitlist_Batchlist );
 
 Waitlist_Batchlist.prototype.onLoad = Waitlist_Load_Query;
 
-Waitlist_Batchlist.prototype.onCreateRootColumnList = function() {
+Waitlist_Batchlist.prototype.onCreateRootColumnList = function()
+{
 	var columnlist =
 	[
 		new MMBatchList_Column_Name( 'Waitlist ID', 'id', 'id')
-		.SetAdvancedSearchEnabled(false)
-		.SetDisplayInMenu(false)
-		.SetDisplayInList(false)
-		.SetAdvancedSearchEnabled(false),
-		new MMBatchList_Column_DateTime( 'Date Added', 'time_added', 'time_added'),
-		new MMBatchList_Column_Name( 'Email', 'email', 'email'),
-		new MMBatchList_Column_Name( 'Product Code', 'product_code', 'product_code'),
-		new MMBatchList_Column_Name( 'Product Name', 'product_name', 'product_name'),
-		new MMBatchList_Column_Name( 'Variant Code', 'variant_code', 'variant_code'),
-		new MMBatchList_Column_Name( 'Variant Product Name', 'variant_name', 'variant_name'),
-		new MMBatchList_Column_Name( 'Product ID', 'product_id', 'product_id')
-		.SetDisplayInMenu(false)
-		.SetDisplayInList(false)
-		.SetAdvancedSearchEnabled(false),
-		new MMBatchList_Column_Name( 'Variant ID', 'variant_id ', 'variant_id')
-		.SetAdvancedSearchEnabled(false)
-		.SetDisplayInMenu(false)
-		.SetDisplayInList(false),
-		new MMBatchList_Column_Name( 'Current Stock', 'inv_count', 'inv_count')
-		.SetAdvancedSearchEnabled(false)
-		.SetSortByField( '' )
+			.SetAdvancedSearchEnabled(false)
+			.SetDisplayInMenu(false)
+			.SetDisplayInList(false)
+			.SetAdvancedSearchEnabled(false)
+			.SetSortByField( '' ),
+		new MMBatchList_Column_DateTime(	'Date Added',			'time_added',	'time_added'),
+		new MMBatchList_Column_Name(		'Email', 'email',		'email'),
+		new MMBatchList_Column_Name(		'Product Code',			'product_code',	'product_code'),
+		new MMBatchList_Column_Name(		'Product Name',			'product_name',	'product_name'),
+		new MMBatchList_Column_Name(		'Variant Code',			'variant_code',	'variant_code'),
+		new MMBatchList_Column_Name(		'Variant Product Name',	'variant_name',	'variant_name'),
+		new MMBatchList_Column_Name(		'Product ID',			'product_id',	'product_id')
+			.SetDisplayInMenu(false)
+			.SetDisplayInList(false)
+			.SetAdvancedSearchEnabled(false)
+			.SetSortByField( '' ),
+		new MMBatchList_Column_Name(		'Variant ID',			'variant_id ',	'variant_id')
+			.SetAdvancedSearchEnabled(false)
+			.SetDisplayInMenu(false)
+			.SetDisplayInList(false)
+			.SetSortByField( '' ),
+		new MMBatchList_Column_Name( 'Current Stock',				'inv_count',	'inv_count')
+			.SetAdvancedSearchEnabled(false)
+			.SetSortByField( '' )
 	];
+
 	return columnlist;
 }
 
-Waitlist_Batchlist.prototype.onDelete = function( item, callback, delegator ) {
+Waitlist_Batchlist.prototype.onDelete = function( item, callback, delegator )
+{
 	Waitlist_Batchlist_Delete( item.record.id, callback, delegator );
 }
 
